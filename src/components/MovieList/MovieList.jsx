396f564a79e4b14f77546e5c2cae8c4547ac8374
Carro <carolinaisabellaandersson@gmail.com>
2023-05-26 13:02:15 +0200

    alot of styling

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,36 +1,40 @@
-import { Link } from "react-router-dom";
-import FilterDropdown from "../FilterDropDown/FilterDropdown";
+import { useState, useEffect } from "react";
 import MovieCard from "../MovieCard/MovieCard";
-import { useEffect, useState } from "react";
+import FilterDropDown from "../FilterDropDown/FilterDropdown";
+import { Link, useSearchParams } from "react-router-dom";
+
 import "./MovieList.css";
 
 function MovieList() {
   const [movies, setMovies] = useState([]);
+  const [filterParams, setFilterParams] = useSearchParams({
+    filter: "popular",
+  });
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const res = await fetch(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
-      const data = await res.json();
-      setMovies(data.results);
-    };
-    fetchMovies();
-  }, []);
-
-  useEffect(() => {
-    console.log(movies);
-  }, [movies]);
+    fetch(
+      `https://api.themoviedb.org/3/movie/${filterParams.get(
+        "filter"
+      )}?api_key=${import.meta.env.VITE_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => setMovies(data.results));
+  }, [filterParams]);
 
   return (
     <div className="movie_list">
       <div className="movie_list_filter">
         <h4>Filter movies</h4>
-        <FilterDropdown />
+        <FilterDropDown
+          filter={filterParams.get("filter")}
+          setFilter={setFilterParams}
+        />
       </div>
-      <h2>Popular Movies</h2>
+      <h2>
+        {filterParams.get("filter") != "top_rated"
+          ? filterParams.get("filter") + " Movies"
+          : "Top Rated Movies"}
+      </h2>
       <div className="movie_grid">
         {movies.map((movie) => (
           <Link to={`/${movie.id}`} key={movie.id}>
--- src/components/MyMovies/MyMovies.jsx
+++ src/components/MyMovies/MyMovies.jsx
@@ -1,10 +1,22 @@
-import "./MyMovies.css";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import { FiEye } from "react-icons/fi";
+import MovieDrawer from "../MovieDrawer/MovieDrawer";
+import { useMovieContext } from "../../context/MyMoviesContext";
+
+import "./MyMovies.css";
+
 function MyMovies() {
+  const [open, setOpen] = useState(false);
+  const { movies, addMovies } = useMovieContext();
   return (
-    <div className="my_movies">
+    <div className="my_movies" onClick={() => setOpen(true)}>
       <p>0</p>
       <FiEye />
+      {movies.map((movie) => (
+        <p key={movie}>{movie}</p>
+      ))}
+      <MovieDrawer open={open} setOpen={setOpen} />
     </div>
   );
 }
